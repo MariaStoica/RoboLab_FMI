3c3677e92cac2428e8728f81a4effcf0f7fe5905
Maria Stoica <maria.stoica13@gmail.com>
2015-10-26 16:28:03 +0200

    cod placa alba

--- arduino_projects/our_code_oct2015/our_code_oct2015.ino
+++ arduino_projects/our_code_oct2015/our_code_oct2015.ino
@@ -5,11 +5,12 @@ int MOTOR2_PIN2 = 5;
 int MOTOR1_PIN1 = 6;
 int MOTOR1_PIN2 = 9;
 
-int s0 = 0; // spate stg
-int s1 = 0; // spate dr
-int s2 = 0; // fata dr
-int s3 = 0; // fata stg
-int s4 = 0; // distanta
+int s0 = 0; // distanta
+int s1 = 0; // left
+int s2 = 0; // right
+int s3 = 0; // culoare spate
+int s4 = 0; // culoare stanga
+int s5 = 0; // culoare dreapta
 
 void setup() {
   // put your setup code here, to run once:
@@ -37,36 +38,37 @@ s1 = analogRead(1);
 s2 = analogRead(2);
 s3 = analogRead(3);
 s4 = analogRead(4);
+s5 = analogRead(5);
 
 //  Serial.println(s0);
 //  Serial.println(s1);
 //  Serial.println(s2);
 //  Serial.println(s3);
 //  Serial.println(s4);
+//  Serial.println(s5);
 //  delay(500);
 
+//go(100,100);
 
-
-  if(s2 < 100 && s3 < 100){ // alb fata complet
-    go(-255,-255);
+  if(s4 < 700 && s5 < 700){ // alb fata complet
+    go(-100,-100);
   }
-  else if(s2 < 100){ // dc alb pe dreapta
-    go(255, -255);
+  else if(s5 < 700){ // dc alb pe dreapta
+    go(100, -100);
     
   }
-  else if(s3 < 100){ // dc alb pe stg
-    go(-255, 255);
+  else if(s4 < 700){ // dc alb pe stg
+    go(-100, 100);
     
   }
-  else if(s4 > 200){ // daca dusman pe teritoriu, da-l afara
-    go(255,255);
+  else if(s0 > 700){ // daca dusman pe teritoriu, da-l afara
+    go(100,100);
   }
   else
   {
-//    go(255,255);
-    go(-255,255);
+    go(100,100);
   }
-  
+//  
 //  go(255, 255);
 //  Serial.println(s4);
 
